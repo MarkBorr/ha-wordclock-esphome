520cdd3a8b7bf95eef9ebc34f27957159c6ab20c
Mark Borrmann <mark.borrmann@icloud.com>
2022-06-22 12:58:12 +0200

    fit code to own needs

--- components/wordclock.h
+++ components/wordclock.h
@@ -26,33 +26,33 @@ int leds_time_it_is[] = {0, 1, 3, 4, 5}; // ES IST
 int leds_minutes[] = {124, 123, 122, 121}; // Minutes LEDS
 int leds_skiped[] = {110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120}; // Check Line 101
 int leds_time_minutes[][15] = {
-    {101, 100,  99,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}, // UHR
-    {  7,   8,   9,  10,  41,  40,  39,  38,  -1,  -1,  -1,  -1,  -1,  -1,  -1}, // FÜNF, NACH
-    { 21,  20,  19,  18,  41,  40,  39,  38,  -1,  -1,  -1,  -1,  -1,  -1,  -1}, // ZEHN, NACH
-    { 26,  27,  28,  29,  30,  31,  32,  41,  40,  39,  38,  -1,  -1,  -1,  -1}, // VIERTEL, NACH
-    { 17,  16,  15,  15,  14,  13,  12,  11,  41,  40,  39,  38,  -1,  -1,  -1}, // ZWANZIG, NACH
-    {  7,   8,   9,  10,  37,  36,  35,  44,  45,  46,  47,  -1,  -1,  -1,  -1}, // FÜNF, VOR, HALB
+    {107, 108,  109,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}, // UHR
+    {  7,   8,   9,  10,  35,  36,  37,  38,  -1,  -1,  -1,  -1,  -1,  -1,  -1}, // FÜNF, NACH
+    { 11,  12,  13,  14,  35,  36,  37,  38,  -1,  -1,  -1,  -1,  -1,  -1,  -1}, // ZEHN, NACH
+    { 26,  27,  28,  29,  30,  31,  32,  35,  36,  37,  38,  -1,  -1,  -1,  -1}, // VIERTEL, NACH
+    { 15,  16,  17,  18,  19,  20,  21,  35,  36,  37,  38,  -1,  -1,  -1,  -1}, // ZWANZIG, NACH
+    {  7,   8,   9,  10,  39,  40,  41,  44,  45,  46,  47,  -1,  -1,  -1,  -1}, // FÜNF, VOR, HALB
     { 44,  45,  46,  47,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}, // HALB
-    {  7,   8,   9,  10,  41,  40,  39,  38,  44,  45,  46,  47,  -1,  -1,  -1}, // FÜNF, NACH, HALB
-    { 17,  16,  15,  15,  14,  13,  12,  11,  37,  36,  35,  -1,  -1,  -1,  -1}, // ZWANZIG, VOR
-    { 22,  23,  24,  25,  26,  27,  28,  29,  30,  31,  32,  -1,  -1,  -1,  -1}, // DREIVIERTEL
-    { 21,  20,  19,  18,  37,  36,  35,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}, // ZEHN, VOR
-    {  7,   8,   9,  10,  37,  36,  35,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}  // FÜNF, VOR
+    {  7,   8,   9,  10,  35,  36,  37,  38,  44,  45,  46,  47,  -1,  -1,  -1}, // FÜNF, NACH, HALB
+    { 15,  16,  17,  18,  19,  20,  21,  39,  40,  41,  -1,  -1,  -1,  -1,  -1}, // ZWANZIG, VOR
+    { 26,  27,  28,  29,  30,  31,  32,  39,  40,  41,  -1,  -1,  -1,  -1,  -1}, // DREIVIERTEL
+    { 11,  12,  13,  14,  39,  40,  41,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}, // ZEHN, VOR
+    {  7,   8,   9,  10,  39,  40,  41,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}  // FÜNF, VOR
 };
 
 int leds_time_hours[][6] = {
     { 49,  50,  51,  52,  53,  -1}, // ZWÖLF
-    { 63,  62,  61,  60,  -1,  -1}, // EINS
-    { 65,  64,  63,  62,  -1,  -1}, // ZWEI
+    { 57,  58,  59,  60,  -1,  -1}, // EINS
+    { 55,  56,  57,  58,  -1,  -1}, // ZWEI
     { 67,  68,  69,  70,  -1,  -1}, // DREI
-    { 80,  79,  78,  77,  -1,  -1}, // VIER
+    { 84,  85,  86,  87,  -1,  -1}, // VIER
     { 73,  74,  75,  76,  -1,  -1}, // FÜNF
-    {108, 107, 106, 105, 104,  -1}, // SECHS
-    { 60,  59,  58,  57,  56,  55}, // SIEBEN
+    {100, 101, 102, 103, 104,  -1}, // SECHS
+    { 60,  61,  62,  63,  64,  65}, // SIEBEN
     { 89,  90,  91,  92,  -1,  -1}, // ACHT
-    { 84,  83,  82,  81,  -1,  -1}, // NEUN
+    { 80,  81,  82,  83,  -1,  -1}, // NEUN
     { 93,  94,  95,  96,  -1,  -1}, // ZEHN
-    { 87,  86,  85,  -1,  -1,  -1}  // ELF
+    { 77,  78,  79,  -1,  -1,  -1}  // ELF
 };
 
 CRGB leds[NUM_LEDS];
@@ -89,14 +89,18 @@ class Wordclock : public Component, public CustomAPIDevice {
             int h = time.hour;
             int m = time.minute;
             // https://www.esphome.io/api/classesphome_1_1light_1_1_light_color_values.html LightColorValues Class
-            auto fastledlight2 = id(fastledlight).current_values;
+            auto fastledlightfg = id(fastledlight_foreground).current_values;
+            auto fastledlightbg = id(fastledlight_background).current_values;
             //convert float 0.0 till 1.0 into int 0 till 255
-            red = (int)(fastledlight2.get_red()*255);
-            green = (int)(fastledlight2.get_green()*255);
-            blue = (int)(fastledlight2.get_blue()*255);
+            red = (int)(fastledlightfg.get_red()*255);
+            green = (int)(fastledlightfg.get_green()*255);
+            blue = (int)(fastledlightfg.get_blue()*255);
             brightness = 0;
+            red2 = (int)(fastledlightbg.get_red()*255);
+            green2 = (int)(fastledlightbg.get_green()*255);
+            blue2 = (int)(fastledlightbg.get_blue()*255);
             //check if light is on and set brightness
-            if (fastledlight2.get_state() > 0 ) { brightness = (int)(fastledlight2.get_brightness()*255); }
+            if (fastledlightfg.get_state() > 0 ) { brightness = (int)(fastledlightfg.get_brightness()*255); }
             else { ESP_LOGD("loop", "fastledlight state off - b: %i rgb %i %i %i", brightness, red, green, blue); delay(500);}
             FastLED.setBrightness(brightness);
             //check if valid time. Blink red,green,blue until valid time is present
@@ -121,7 +125,7 @@ class Wordclock : public Component, public CustomAPIDevice {
                         tmp_hour = tmp_hour % 12;
                         int minutessum = minute % 5;
                         // Reset all LED, but skip LED 110 till 120
-                        for(int i = 0; i < NUM_LEDS; i++) {     if(i < 110 || i > 120) leds[i].setRGB(0, 0, 0); }
+                        for(int i = 0; i < NUM_LEDS; i++) {     if(i < 110 || i > 120) leds[i].setRGB(red2, green2, blue2); }
                         for(int i = 0; i < 5; i++) {            leds[leds_time_it_is[i]].setRGB(red, green, blue); }
                         for(int i = 0; i < 15; i++) {           if(leds_time_minutes[tmp_minute][i] >= 0) { leds[leds_time_minutes[tmp_minute][i]].setRGB(red, green, blue); } }
                         for(int i = 0; i < 6; i++) {            if(leds_time_hours[tmp_hour][i] >= 0) { leds[leds_time_hours[tmp_hour][i]].setRGB(red, green, blue); } }
